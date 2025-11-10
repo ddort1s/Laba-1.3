31407f542994f4d6378f361f3d6d73a4fa70a6a5
ddort1s <141556284+ddort1s@users.noreply.github.com>
2025-11-10 16:12:21 +0300

    Update Main2.java
    New Commit

--- src/Main2.java
+++ src/Main2.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 
 public class Main2 {
-    public static void main(String []args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество чисел: ");
         int n = scanner.nextInt();
-
-        Integer minNechet = null;
-
+        
+        int minNechet = 0;
+        boolean found = false;
+        
         for (int i = 1; i <= n; i++) {
             System.out.print("Введите число " + i + ": ");
             int num = scanner.nextInt();
-
-            if (num%2!=0) {
-                if (minNechet == null || num < minNechet) {
+            
+            if (num % 2 != 0) {
+                if (!found || num < minNechet) {
                     minNechet = num;
+                    found = true;
                 }
             }
         }
-        if (minNechet != null) {
-            System.out.print(minNechet);
-        }
-        else {
-            System.out.print("undefined");
+        
+        if (found) {
+            System.out.println(minNechet);
+        } else {
+            System.out.println("undefined");
         }
     }
 }
